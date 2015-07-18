07860d20dedcf8db2c5011eabe78bb4e4e47205a
heyheyjose <heyheyjose@users.noreply.github.com>
2015-07-18 15:26:37 -0400

    more updates

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,13 @@
 var React = require('react');
 
 var App = React.createClass({
-	render() {
-		return <h1>This is my own React dev starter kit :)</h1>;
+
+	render: function () {
+		return (
+			<h1>This is my own React dev starter kit :)</h1>
+		);
 	}
-})
+
+});
 
 React.render(<App/>, document.getElementById('example'));
